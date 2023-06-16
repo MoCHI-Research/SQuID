c8f59fc1613db8eba8bb088fe2930d1e394ce5d7
Matthew <mmaillet@hamilton.edu>
2023-06-16 10:23:00 -0400

    GPT can now output things into a CSV file
    Created "convert_gpt_to_csv" and updated "label_datapoints" functions

--- menu.py
+++ menu.py
@@ -3,9 +3,42 @@ from gptconnection import openai_chatcompletion
 import csv
 import os
 
+def convert_gpt_to_csv(input_string):
+    # Convert the input_string into lines
+    lines = input_string.split('\n')
 
+    group_name = ''
+    group_data = []
+    csv_data = []
 
+    # Parse the lines
+    for line in lines:
+        line = line.strip()
+        if line.startswith('Group'):
+            if group_name:
+                csv_data.append((group_name, group_data))
+            group_name = line.split(':', 1)[1].strip()
+            group_data = []
+        elif line.startswith('-'):
+            group_data.append(line[2:])  # Skip the leading "- "
+
+    if group_name and group_data:  # Add the last group
+        csv_data.append((group_name, group_data))
+
+    # Write to CSV
+    with open('output.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Group', 'Items'])
+        for data in csv_data:
+            group = data[0]
+            for item in data[1]:
+                writer.writerow([group, item])
+    print("Successfully created 'output.csv'")
+
+# Grabs the data points from a CSV file and asks GPT to sort them by group labels like an affinity diagram
 def label_datapoints(file):
+    print("Generating GPT response . . .\n")
+    
     gpt_template = "Group the following data based on similarity using as many groups as you find appropriate:\n"
     with open(file, newline='') as f:
         reader = csv.reader(f)
@@ -15,7 +48,10 @@ def label_datapoints(file):
             if count == 10:
                 break
             count += 1
-    print(openai_chatcompletion(gpt_template))
+    response = openai_chatcompletion(gpt_template)
+    print('Successfully generated GPT response.\nGenerating CSV file . . .')
+    
+    convert_gpt_to_csv(response)
 
 # A function that accepts file inputs and returns the inputted file
 def file_input():
