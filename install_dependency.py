55e8697d37d41fede0980d8f679819a48e34e7e1
unknown <instantsages@gmail.com>
2023-10-14 22:54:51 -0400

    Made scripts to install dependency

---
+++ install_dependency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+def main():
+    package_list = ["pandas", "matplotlib", "plotly", "python-dotenv", "customtkinter", "scikit-learn", "networkx", "openai"]
+    for package_name in package_list:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
+
+
+
+
+if __name__ == "__main__":
+    main()
