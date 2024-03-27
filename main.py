d58ab4554c36184fe82aff9e0e8c3d2e42d6ea26
unknown <instantsages@gmail.com>
2024-03-27 12:47:40 -0400

    Removed key at start and end

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from ui import App
+import os
 
 def main():
+    os.environ["OPENAI_API_KEY"] = ""
     app = App()
     app.wm_minsize(width=1200, height=700)
     app.mainloop()
+    os.environ["OPENAI_API_KEY"] = ""
 
 if __name__ == "__main__":
     main()
